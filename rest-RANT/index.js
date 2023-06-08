33f98e4578990a50d285e90b8df0cc544aaa7fff
kmumfor2 <120679405+kmumfor2@users.noreply.github.com>
2023-06-07 23:03:44 -0400

    Adding places view with mock data

--- rest-RANT/index.js
+++ rest-RANT/index.js
@@ -15,6 +15,26 @@ app.get('*', (req, res) => {
   res.render('error404')
 })
 
+// GET /places
+app.get('/', (req, res) => {
+    let places = [{
+        name: 'H-Thai-ML',
+        city: 'Seattle',
+        state: 'WA',
+        cuisines: 'Thai, Pan-Asian',
+        pic: 'http://placekitten.com/250/250'
+      }, {
+        name: 'Coding Cat Cafe',
+        city: 'Phoenix',
+        state: 'AZ',
+        cuisines: 'Coffee, Bakery',
+        pic: 'http://placekitten.com/250/250'
+      }]
+      
+      res.render('places/index', { places })
+
+  })
+
 app.listen(process.env.PORT)
 
 
---
+++ rest-RANT/views/places/error404.jsx
@@ -0,0 +1,6 @@
+const React = require('react')
+const Def = require('./default')
+
+function error404 () {}
+
+module.exports = error404
