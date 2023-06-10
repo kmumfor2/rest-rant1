a9b688804985ddcd7490986b40436fb3e025dc32
kmumfor2 <120679405+kmumfor2@users.noreply.github.com>
2023-06-10 14:19:14 -0400

    Added the edit form

--- rest-RANT/views/places/edit.jsx
+++ rest-RANT/views/places/edit.jsx
@@ -1,15 +1,15 @@
 const React = require('react')
 const Def = require('../default.jsx')
 
-function new_form () {
+function edit_form (data) {
     return (
         <Def>
           <main>
-            <h1>Add a New Place</h1>
-            <form method="POST" action="/places">
+            <h1>Edit Place</h1>
+            <form method="POST" action={`/places/${data.place.id}?_method=PUT`}>
                   <div className="form-group">
                     <label htmlFor="name">Place Name</label>
-                    <input className="form-control" id="name" name="name" required />
+                    <input className="form-control" id="name" name="name" value ={data.place.name} required />
                   </div>
                   <div className="form-group">
                     <label htmlFor="pic">Place Picture</label>
@@ -28,14 +28,13 @@ function new_form () {
                     <input className="form-control" id="cuisines" name="cuisines" required />
                   </div>
                   <input className="btn btn-primary" type="submit" value="Add Place" />
-                </form>
+            </form>
 
           </main>
+          
         </Def>
     )
 }
 
-module.exports = new_form
 
-
-
+module.exports = edit_form
