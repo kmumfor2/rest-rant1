b042ba4fd8893c499c84730d9501584ef8b08d02
kmumfor2 <120679405+kmumfor2@users.noreply.github.com>
2023-06-10 13:26:36 -0400

    New places can now be added

--- rest-RANT/controllers/places.js
+++ rest-RANT/controllers/places.js
@@ -6,10 +6,21 @@
       res.render('places/index', { places })
   })
   
-router.post('/', (req, res) => {
-    console.log(req.body)
-    res.send('POST /places')
+  router.post('/', (req, res) => {
+    if (!req.body.pic) {
+      // Default image if one is not provided
+      req.body.pic = 'http://placekitten.com/400/400'
+    }
+    if (!req.body.city) {
+      req.body.city = 'Anytown'
+    }
+    if (!req.body.state) {
+      req.body.state = 'USA'
+    }
+    places.push(req.body)
+    res.redirect('/places')
   })
+  
   
 let places = [{
     name: 'H-Thai-ML',
