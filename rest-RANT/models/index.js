62da5eeff0db27f18c4d8d2e55a10a8879dcd192
kmumfor2 <120679405+kmumfor2@users.noreply.github.com>
2023-06-11 13:11:16 -0400

    Added a comments section

--- rest-RANT/models/index.js
+++ rest-RANT/models/index.js
@@ -1,7 +1,10 @@
-require ('dotenv').config()
+require('dotenv').config()
 const mongoose = require('mongoose')
 
 mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true, 
   useUnifiedTopology: true
 })
+
+module.exports.Place = require('./places')
+module.exports.Comment = require('./comment')
--- rest-RANT/views/places/show.jsx
+++ rest-RANT/views/places/show.jsx
@@ -2,31 +2,40 @@ const React = require('react')
 const Def = require('../default')
 
 function show (data) {
+    let comments = (
+      <h3 className="inactive">
+        No comments yet!
+      </h3>
+    )
+    if (data.place.comments.length) {
+      comments = data.place.comments.map(c => {
+        return (
+          <div className="border">
+            <h2 className="rant">{c.rant ? 'Rant! ðŸ˜¡' : 'Rave! ðŸ˜»'}</h2>
+            <h4>{c.content}</h4>
+            <h3>
+              <stong>- {c.author}</stong>
+            </h3>
+            <h4>Rating: {c.stars}</h4>
+          </div>
+        )
+      })
+    }
     return (
         <Def>
           <main>
-            <h1>{ data.place.name }</h1>
-            <h2>currently unrated</h2>
-            <h3>
-                Located in {data.place.city}, {data.place.state}
-            </h3>
-            <h3>{data.place.showEstablished}</h3>
-            <h4>
-                Serving {data.place.cuisines}
-            </h4>
-            <a href={`/places/${data.id}/edit`} className="btn btn-warning"> 
-                Edit
-            </a>     
-            <form method="POST" action={`/places/${data.id}?_method=DELETE`}> 
-                <button type="submit" className="btn btn-danger">
-                    Delete
-                </button>
-            </form> 
-
+            <div className="row">
+              ...
+            </div>
+            <hr />
+            <h2>Comments</h2>
+            {comments}
           </main>
         </Def>
     )
 }
+
+module.exports = show
 
 
 
